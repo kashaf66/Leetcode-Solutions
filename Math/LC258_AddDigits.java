de600f599b85b5fc9a3d5585dc7db27e4278814a
kashaf mirza <kashafmirza664@gmail.com>
2026-06-10 23:07:54 +0530

    added palindrome solution

--- Math/LC258_AddDigits.java
+++ Math/LC258_AddDigits.java
@@ -1,19 +1,39 @@
 public class LC258_AddDigits {
 
-    public int addDigits(int num) {
+    // public int addDigits(int num) {
 
-        while (num >= 10) {
-            int sum = 0;
+    //     while (num >= 10) {
+    //         int sum = 0;
 
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
+    //         while (num > 0) {
+    //             sum += num % 10;
+    //             num /= 10;
+    //         }
 
-            num = sum;
-        }
+    //         num = sum;
+    //     }
 
-        return num;
+    //     return num;
+    // }
+    public boolean isUgly(int n) {
+       if(n<=0) {
+        return false;
+       }
+       while(n%2==0){
+        n/=2;
+       }
+       while(n%3==0){
+        n/=3;
+       }
+       while(n%5==0){
+        n/=5;
+       }
+       return n==1;
+    }
+    public static void main(String[] args) {
+        LC258_AddDigits obj = new LC258_AddDigits();
+
+        System.out.println(obj.isUgly(9));
     }
 }
 
