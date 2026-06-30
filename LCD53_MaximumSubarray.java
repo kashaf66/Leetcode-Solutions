c12c7b015398d633be54fb77e72089174286c7bb
kashaf mirza <kashafmirza664@gmail.com>
2026-07-01 00:44:55 +0530

    kadane,s algo

---
+++ LCD53_MaximumSubarray.java
@@ -0,0 +1,16 @@
+public class LCD53_MaximumSubarray {
+     public int maxSubArray(int[] nums) {
+    int n =nums.length;
+    int maxi=Integer.MIN_VALUE;
+    int sum=0;
+    for (int i =0;i<n;i++){
+        sum+=nums[i];
+        maxi=Math.max(maxi,sum);
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return maxi;
+    }
+}
+
