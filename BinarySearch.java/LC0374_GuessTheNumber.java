8071e326db6276962ada275e6b12953ec28dc614
kashaf mirza <kashafmirza664@gmail.com>
2026-07-29 00:20:44 +0530

    solved leetcode-374

---
+++ BinarySearch.java/LC0374_GuessTheNumber.java
@@ -0,0 +1,24 @@
+public class LC0374_GuessTheNumber {
+    public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int low = 1;
+        int high = n;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int res = guess(mid);
+
+            if (res == 0) {
+                return mid;
+            } else if (res == 1) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+}
