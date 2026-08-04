c133263aef630f38dcc41ab1fbe54ce6f518936d
kashaf mirza <kashafmirza664@gmail.com>
2026-08-04 22:06:10 +0530

    solved leetcode-1482

---
+++ BinarySearch.java/LC1482_MinNumOfDaysToMakeM_Bouquets.java
@@ -0,0 +1,41 @@
+class Solution {
+     public boolean canMake(int[] bloomDay,int day,int m ,int k){
+        int flowers=0;
+        int bouquets=0;
+        for(int bloom:bloomDay){
+            if(bloom<=day){
+                flowers++;
+
+                if(flowers==k){
+                    bouquets++;
+                    flowers=0;
+                }
+            }else{
+              flowers=0;
+            }
+        }
+        return bouquets>=m;
+     } 
+    public int minDays(int[] bloomDay, int m, int k) {
+        if ((long) m * k > bloomDay.length)
+            return -1;
+
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        for (int day : bloomDay) {
+            low = Math.min(low, day);
+            high = Math.max(high, day);
+        }
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+            if (canMake(bloomDay, mid, m, k)) {
+                high = mid - 1; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+
+        return low;
+    }
+}
