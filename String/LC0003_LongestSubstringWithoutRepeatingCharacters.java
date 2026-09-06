37ae49f2b5330e8e8da43a7fa2580680f30ea08b
kashaf66 <kashafmirza664@gmail.com>
2026-09-07 01:02:04 +0530

    solved Leetcode-03

---
+++ String/LC0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+public class LC0003_LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        boolean[]seen=new boolean[256];
+        int left=0;
+        int maxlen=0;
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            while(seen[ch]){
+                seen[s.charAt(left)]=false;
+                left++;
+            }
+            seen[ch]=true;
+            maxlen=Math.max(maxlen,right-left+1);
+        
+        }
+    return maxlen;
+    }
+}
