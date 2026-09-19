5ed85a3f30bfef98710a4ff98b49020ce3ac4ffa
kashaf66 <kashafmirza664@gmail.com>
2026-09-19 22:42:39 +0530

    solved leetcode-160

---
+++ LinkedList/LC0160_IntersectionOfTwoLL.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class LC0160_IntersectionOfTwoLL {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null){
+            return null;
+        }
+        ListNode t1=headA;
+        ListNode t2=headB;
+      while(t1!=t2){
+
+        t1 = (t1 == null) ? headB : t1.next;
+
+        t2 = (t2 == null) ? headA : t2.next;
+
+      }
+      return t1;
+    }
+}
