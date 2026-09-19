40208e0ef80909ad3977237c6e01fa660aa57b27
kashaf66 <kashafmirza664@gmail.com>
2026-09-19 22:40:03 +0530

    solved leetcode-142

---
+++ LinkedList/LC0142LinkedListCycle2.java
@@ -0,0 +1,24 @@
+package LinkedList;
+
+public class LC0142LinkedListCycle2 {
+     public ListNode detectCycle(ListNode head) {
+          ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+            
+             ListNode temp = head;
+
+                while (temp != slow) {
+
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                return temp;
+            }
+        }
+        return null;
+    }
+}
